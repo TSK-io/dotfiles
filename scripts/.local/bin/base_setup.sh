103d1518800abbaa1e12773d2030c66f83d1f830
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-02-26 14:51:10 -0500

    update

--- scripts/.local/bin/base_setup.sh
+++ scripts/.local/bin/base_setup.sh
@@ -27,5 +27,7 @@ command -v repomix > /dev/null || sudo npm install -g repomix
 # helix
 command -v hx > /dev/null || (wget https://github.com/helix-editor/helix/releases/download/25.07.1/helix_25.7.1-1_amd64.deb && sudo apt -y install ./helix_25.7.1-1_amd64.deb && rm helix_25.7.1-1_amd64.deb)
 
+# zz
+mkdir -p ~/.local/bin
 command -v zz > /dev/null || wget -O ~/.local/bin/zz https://github.com/TSK-io/zz_translator/releases/download/v0.1.0/zz
 chmod +x ~/.local/bin/zz
