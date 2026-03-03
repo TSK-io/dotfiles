aba029e5eaa4cddecab2e7aa5396fa15c40257ba
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-03-03 18:23:30 -0500

    update

--- mpd/scripts/install_mpd.sh
+++ mpd/scripts/install_mpd.sh
@@ -1,8 +1,8 @@
 # 1. 仅安装核心服务端
 apt update && apt install mpd -y
 
-# 2. 从你的 dotfiles 复制配置文件过去
-cp ~/dotfiles/mpd/mpd.conf /etc/mpd.conf
+# 2. 强制创建软链接 (-s 是软链接，-f 是如果存在就强制覆盖)
+ln -sf ~/dotfiles/mpd/mpd.conf /etc/mpd.conf
 
 # 3. 赋予音乐目录权限并重启服务
 chmod -R 755 /mnt/data/Music
