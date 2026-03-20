69a456cba2612216328d7fd26c1de18a17c929e6
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-03-20 19:37:05 +0000

    update

--- scripts/.local/bin/desktop_setup.sh
+++ scripts/.local/bin/desktop_setup.sh
@@ -10,11 +10,6 @@ sudo apk add keyd
 sudo ln -sf $HOME/dotfiles/keyd/default.conf /etc/keyd/
 sudo keyd
 
-# qemu
-sudo apt -y install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
-sudo adduser $USER libvirt
-sudo adduser $USER kvm
-
 # fcitx5 # Restart Required
 sudo apt -y install fcitx5 fcitx5-chinese-addons fcitx5-frontend-gtk3 fcitx5-frontend-qt5 im-config
 im-config -n fcitx5
