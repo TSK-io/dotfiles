ee7e75dda4786ddd5947dc2ea66918db2db8886c
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-03-20 19:39:30 +0000

    update

--- scripts/.local/bin/desktop_setup.sh
+++ scripts/.local/bin/desktop_setup.sh
@@ -9,7 +9,3 @@ sudo apk add dunst xclip maim dbus-x11 xinit i3status i3lock i3wm xorg-server
 sudo apk add keyd
 sudo ln -sf $HOME/dotfiles/keyd/default.conf /etc/keyd/
 sudo keyd
-
-# fcitx5 # Restart Required
-sudo apt -y install fcitx5 fcitx5-chinese-addons fcitx5-frontend-gtk3 fcitx5-frontend-qt5 im-config
-im-config -n fcitx5
