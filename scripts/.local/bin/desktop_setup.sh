e094f9627d0bc7b6530dc160a18f01fa17bdc108
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-03-20 19:38:49 +0000

    update

--- scripts/.local/bin/desktop_setup.sh
+++ scripts/.local/bin/desktop_setup.sh
@@ -3,7 +3,7 @@
 # update && upgrade
 apk upgrade && apk upgrade
 
-sudo apk add alacritty dunst xclip maim dbus-x11 xinit i3status i3lock i3wm xorg-server
+sudo apk add dunst xclip maim dbus-x11 xinit i3status i3lock i3wm xorg-server
 
 # keyd
 sudo apk add keyd
