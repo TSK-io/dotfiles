4d037247aa8558b76b1d9696e38504403ba6ca7a
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-04-21 06:37:31 +0800

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,14 +8,10 @@ end
 
 set -gx EDITOR (command -v hx)
 set -gx VISUAL (command -v hx)
-set -gx LOCALSTACK_HOST 127.0.0.1
-set -gx DBUS_SESSION_BUS_ADDRESS "unix:path=/run/user/"(id -u)"/bus"
 set -Ua fish_features no-keyboard-protocols
 
 fish_add_path /usr/sbin
 fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.cargo/bin
-fish_add_path ~/go/bin
 fish_add_path /usr/local/bin
 
 alias cat 'bat --paging=never --style="plain"'
@@ -30,10 +26,5 @@ alias ww 'cd /mnt/c/Users/f*/'
 alias uu 'sudo apt update && sudo apt full-upgrade -y'
 alias gg 'git add . && git commit -m "update" && git pull --rebase && git push'
 alias nano hx
-alias docker podman
-
-function aa
-    tmux save-buffer - | python3 $HOME/dotfiles/docs/chats/scripts/openrouter/openrouter_client_via_openai_sdk.py
-end
 
 starship init fish | source
