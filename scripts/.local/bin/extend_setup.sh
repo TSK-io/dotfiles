ecfa11fdc67ed2706e89cf71d189f1d81345f438
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-04-25 18:48:31 -0400

    update

--- scripts/.local/bin/extend_setup.sh
+++ scripts/.local/bin/extend_setup.sh
@@ -36,7 +36,7 @@ command -v daed > /dev/null || { wget https://github.com/daeuniverse/daed/releas
 sudo systemctl enable --now daed
 
 # zen
-curl -fsSL https://github.com/zen-browser/updates-server/raw/refs/heads/main/install.sh | $SHELL
+curl -fsSL https://github.com/zen-browser/updates-server/raw/refs/heads/main/install.sh | bash
 
 # FlClash
 sudo apt -y install libayatana-appindicator3-dev libkeybinder-3.0-dev
