68ac590608509091ebe72b6b1d7c32cfcdc95dda
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-04-26 05:10:45 +0800

    Update extend_setup.sh

--- scripts/.local/bin/extend_setup.sh
+++ scripts/.local/bin/extend_setup.sh
@@ -6,13 +6,13 @@ sudo apt update && sudo apt upgrade -y
 # xorg-and-dm
 sudo apt install -y xorg lightdm lightdm-gtk-greeter
 
-# i3wm-core (保留了 i3lock)
+# i3wm-core 
 sudo apt install -y i3-wm i3status i3lock suckless-tools x11-xserver-utils
 
-# desktop-apps (去掉了 picom, feh, lxappearance)
+# desktop-apps 
 sudo apt install -y alacritty pcmanfm firefox-esr pavucontrol pulseaudio
 
-# fonts (去掉了 fonts-font-awesome)
+# fonts 
 sudo apt install -y fonts-noto fonts-wqy-microhei
 
 # default-terminal
@@ -20,7 +20,6 @@ command -v alacritty > /dev/null && sudo update-alternatives --set x-terminal-em
 
 # enable-services
 sudo systemctl enable lightdm
-sudo systemctl enable NetworkManager
 
 # clean
 sudo apt autoremove -y && sudo apt clean
