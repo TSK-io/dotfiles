06e6d88825906b84ed320c7cc1e8643bb08e4498
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-04-26 05:02:35 +0800

    Update extend_setup.sh

--- scripts/.local/bin/extend_setup.sh
+++ scripts/.local/bin/extend_setup.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+# debian
+sudo apt update && sudo apt upgrade -y
+
+# xorg-and-dm
+sudo apt install -y xorg lightdm lightdm-gtk-greeter
+
+# i3wm-core
+sudo apt install -y i3-wm i3status i3lock suckless-tools x11-xserver-utils
+
+# desktop-apps
+sudo apt install -y alacritty pcmanfm feh picom firefox-esr lxappearance network-manager-gnome pavucontrol pulseaudio
+
+# fonts
+sudo apt install -y fonts-noto fonts-font-awesome fonts-wqy-microhei
+
+# default-terminal
+command -v alacritty > /dev/null && sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
+
+# enable-services
+sudo systemctl enable lightdm
+sudo systemctl enable NetworkManager
+
+# clean
+sudo apt autoremove -y && sudo apt clean
