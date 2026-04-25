37f78e2078033b280534353c939b4606bcd6fe6c
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-04-26 05:40:02 +0800

    Update base_desktop_setup.sh

--- scripts/.local/bin/base_desktop_setup.sh
+++ scripts/.local/bin/base_desktop_setup.sh
@@ -3,17 +3,19 @@
 # debian
 sudo apt update && sudo apt upgrade -y
 
-# xorg-and-dm
-sudo apt install -y xorg lightdm lightdm-gtk-greeter
-
-# i3wm-core 
-sudo apt install -y i3-wm i3status i3lock rofi x11-xserver-utils
-
-# desktop-apps 
-sudo apt install -y firefox-esr
-
-# fonts 
-sudo apt install -y fonts-noto fonts-wqy-microhei
+# base-desktop-debian
+sudo apt install -y \
+xorg \
+lightdm \
+lightdm-gtk-greeter \
+i3-wm \
+i3status \
+i3lock \
+rofi \
+x11-xserver-utils \
+firefox-esr \
+fonts-noto \
+fonts-wqy-microhei
 
 # default-terminal
 sudo apt install -y alacritty && command -v alacritty > /dev/null && sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
