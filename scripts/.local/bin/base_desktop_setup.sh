14a54347ddf8daf3ce246ba9ef5d512d6407c07b
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-04-26 05:57:37 +0800

    Update base_desktop_setup.sh

--- scripts/.local/bin/base_desktop_setup.sh
+++ scripts/.local/bin/base_desktop_setup.sh
@@ -17,6 +17,10 @@ firefox-esr \
 fonts-noto \
 fonts-wqy-microhei
 
+# fcitx5 # Restart Required
+sudo apt -y install fcitx5 fcitx5-chinese-addons fcitx5-frontend-gtk3 fcitx5-frontend-qt5 im-config
+im-config -n fcitx5
+
 # default-terminal
 sudo apt install -y alacritty && command -v alacritty > /dev/null && sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 
