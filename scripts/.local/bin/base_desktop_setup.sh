7339371aa01334d128b5c593f63b577f4305dc76
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-04-26 06:01:08 +0800

    Update base_desktop_setup.sh

--- scripts/.local/bin/base_desktop_setup.sh
+++ scripts/.local/bin/base_desktop_setup.sh
@@ -24,5 +24,23 @@ im-config -n fcitx5
 # default-terminal
 sudo apt install -y alacritty && command -v alacritty > /dev/null && sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 
+# v2rayA
+wget -qO - https://apt.v2raya.org/key/public-key.asc | sudo tee /etc/apt/keyrings/v2raya.asc
+echo "deb [signed-by=/etc/apt/keyrings/v2raya.asc] https://apt.v2raya.org/ v2raya main" | sudo tee /etc/apt/sources.list.d/v2raya.list
+sudo apt -y update
+sudo apt -y install v2raya v2ray 
+sudo systemctl enable --now v2raya
+
+# daed (Official script)
+command -v daed > /dev/null || { wget https://github.com/daeuniverse/daed/releases/download/v1.24.0/installer-daed-linux-x86_64.deb && sudo apt -y install ./installer-daed-linux-x86_64.deb && rm installer-daed-linux-x86_64.deb; }
+sudo systemctl enable --now daed
+
+# zen
+curl -fsSL https://github.com/zen-browser/updates-server/raw/refs/heads/main/install.sh | $SHELL
+
+# FlClash
+sudo apt -y install libayatana-appindicator3-dev libkeybinder-3.0-dev
+command -v FlClash > /dev/null || { wget https://github.com/chen08209/FlClash/releases/download/v0.8.92/FlClash-0.8.92-linux-amd64.deb && sudo apt -y install ./FlClash-0.8.92-linux-amd64.deb && rm FlClash-0.8.92-linux-amd64.deb; }
+
 # clean
 sudo apt autoremove -y && sudo apt clean
