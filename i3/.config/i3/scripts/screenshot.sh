c30e0ff75f1076ba4edf106059a6aedbe8519342
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-05-12 14:25:40 -0400

    update

--- i3/.config/i3/scripts/screenshot.sh
+++ i3/.config/i3/scripts/screenshot.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-scrot -fs - | xclip -selection clipboard -t image/png
+
+dir="$HOME/.screenshots"
+n=1
+
+mkdir -p "$dir"
+
+while [ -e "$dir/$n.png" ]; do
+	n=$((n + 1))
+done
+
+file="$dir/$n.png"
+
+scrot -fs "$file"
+xclip -selection clipboard -t image/png -i "$file"
