80bb82e74dec9c2be05d14cecc02902e09ab719d
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-05-12 15:14:55 -0400

    update

--- i3/.config/i3/scripts/toggle-pcmanfm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if i3-msg -t get_tree | jq -e '.. | objects | select(.window_properties.class == "Pcmanfm")' >/dev/null; then
-	i3-msg '[class="^Pcmanfm$"] kill' >/dev/null
-else
-	pcmanfm -n "$HOME" >/dev/null 2>&1 &
-fi
