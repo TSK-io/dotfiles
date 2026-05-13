efabf998d28b64ca861039e5c52567a55b00d45c
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-05-13 17:17:39 -0400

    update

---
+++ icewm/.icewm/scripts/fullscreen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dir="/tmp/img"
+n=1
+
+mkdir -p "$dir"
+
+while [ -e "$dir/$n.png" ]; do
+	n=$((n + 1))
+done
+
+file="$dir/$n.png"
+
+scrot "$file"
+xclip -selection clipboard -t image/png -i "$file"
