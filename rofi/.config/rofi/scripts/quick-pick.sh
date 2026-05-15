3c082449c815448eea3c81ff5cab8f1853c986a6
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-05-14 21:02:51 -0400

    update

--- rofi/.config/rofi/scripts/quick-pick.sh
+++ rofi/.config/rofi/scripts/quick-pick.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+# Usage: quick-pick.sh <file> <prompt-label>
+# Reads `---`-separated entries from <file>, lets the user pick via rofi,
+# and copies the selection to the clipboard.
 
-file="$HOME/.config/rofi/scripts/quick-prompts.txt"
+file="$1"
+label="$2"
 
 sel=$({
   item=''
@@ -13,6 +17,6 @@ sel=$({
     fi
   done < "$file"
   [ -n "${item//[$' \t\r\n']}" ] && printf '%s\0' "${item%$'\n'}"
-} | rofi -dmenu -i -sep '\0' -p prompts)
+} | rofi -dmenu -i -sep '\0' -p "$label")
 
 [ -n "$sel" ] && printf '%s' "$sel" | xclip -selection clipboard -in
