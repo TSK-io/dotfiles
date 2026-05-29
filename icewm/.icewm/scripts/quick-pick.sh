64b359bf0b7a26437d9a16d6fa6efeb4d00987cd
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-05-29 15:06:08 -0400

    update

--- icewm/.icewm/scripts/quick-pick.sh
+++ icewm/.icewm/scripts/quick-pick.sh
@@ -29,7 +29,7 @@ sel=$({
 ---
 易懂的话总结这个udemy课程简短的一段话:
 ---
-你帮我扫描nodeseek.com | linux.do | v2ex.com 今天技巧,教程,情报,技术等有含金量的帖子(搜索时间仅限近72小时)
+你帮我扫描 nodeseek.com | linux.do | v2ex.com 今天有含金量的帖子 (搜索时间仅限近24小时)
 ---
 PROMPTS
   [ -n "${item//[$' \t\r\n']}" ] && printf '%s\0' "${item%$'\n'}"
