76f16df019e586fff9bf3f597723a7323267c2a7
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-06-10 20:06:55 -0400

    init commit

--- scripts/.local/bin/extend_setup.sh
+++ scripts/.local/bin/extend_setup.sh
@@ -29,24 +29,11 @@ im-config -n fcitx5
 # default-terminal
 sudo apt install -y alacritty && command -v alacritty > /dev/null && sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 
-# v2rayA
-wget -qO - https://apt.v2raya.org/key/public-key.asc | sudo tee /etc/apt/keyrings/v2raya.asc
-echo "deb [signed-by=/etc/apt/keyrings/v2raya.asc] https://apt.v2raya.org/ v2raya main" | sudo tee /etc/apt/sources.list.d/v2raya.list
-sudo apt -y update
-sudo apt -y install v2raya v2ray 
-sudo systemctl enable --now v2raya
-
 # zen
 [ -d "$HOME/.tarball-installations/zen" ] || curl -fsSL https://github.com/zen-browser/updates-server/raw/refs/heads/main/install.sh | bash
 
 # vscode
 command -v code > /dev/null || { echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections && wget -O /tmp/vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y /tmp/vscode.deb && rm /tmp/vscode.deb; }
-
-# ANTIGRAVITY CLI
-curl -fsSL https://antigravity.google/cli/install.sh | bash
-
-# COPILOT CLI
-curl -fsSL https://gh.io/copilot-install | bash
 
 # CODEX CLI
 npm i -g @openai/codex
